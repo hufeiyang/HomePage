d37bd1ff42319686914df0022df26eb40e5f2149
hufy@akulaku.com <hufy@akulaku.com>
2020-09-28 21:27:39 +0800

    HomeActivity添加路由；添加路由跳转到购物车的逻辑

--- module_home/src/main/java/com/hfy/module_home/HomeActivity.java
+++ module_home/src/main/java/com/hfy/module_home/HomeActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+
+@Route(path = "homepage/homeActivity")
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -16,7 +20,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //通过路由跳转到 购物车组件的购物车页面（但没有依赖购物车组件）
-
+                ARouter.getInstance().build("/cart/cartActivity").navigation();
             }
         });
     }
