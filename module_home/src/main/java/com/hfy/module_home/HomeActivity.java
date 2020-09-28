29ce49da60fa07a2a071c2c5bd8d913eaf1967d8
hufy@akulaku.com <hufy@akulaku.com>
2020-09-28 21:03:33 +0800

    Initial commit

---
+++ module_home/src/main/java/com/hfy/module_home/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.hfy.module_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        findViewById(R.id.btn_go_cart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //通过路由跳转到 购物车组件的购物车页面（但没有依赖购物车组件）
+
+            }
+        });
+    }
+}
