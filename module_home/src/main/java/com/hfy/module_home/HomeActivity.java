065b93a9a2d57655ac2eb6f91c4147b2251cf2e4
hufy@akulaku.com <hufy@akulaku.com>
2020-09-30 16:15:00 +0800

    调用购物车组件服务

--- module_home/src/main/java/com/hfy/module_home/HomeActivity.java
+++ module_home/src/main/java/com/hfy/module_home/HomeActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.hfy.export_cart.CartServiceUtil;
 
 @Route(path = "/homepage/homeActivity")
 public class HomeActivity extends AppCompatActivity {
@@ -16,12 +18,22 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //跳转到购物车页面
         findViewById(R.id.btn_go_cart).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //通过路由跳转到 购物车组件的购物车页面（但没有依赖购物车组件）
-                ARouter.getInstance().build("/cart/cartActivity").navigation();
+                ARouter.getInstance()
+                        .build("/cart/cartActivity")
+                        .withString("key1","value1")//携带参数1
+                        .withString("key2","value2")//携带参数2
+                        .navigation();
             }
         });
+
+
+        //调用购物车组件服务：获取购物车商品数量
+        TextView tvCartProductCount = findViewById(R.id.tv_cart_product_count);
+        tvCartProductCount.setText("购物车商品数量:"+ CartServiceUtil.getCartProductCount());
     }
 }
