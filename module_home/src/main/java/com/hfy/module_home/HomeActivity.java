6ee76d529ae740253ace2f15bcd2ccae41d192ca
hufy@akulaku.com <hufy@akulaku.com>
2020-10-02 19:44:35 +0800

    获取购物车组件的fragment实例

--- module_home/src/main/java/com/hfy/module_home/HomeActivity.java
+++ module_home/src/main/java/com/hfy/module_home/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.hfy.module_home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +12,7 @@ import android.widget.TextView;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.hfy.export_cart.CartServiceUtil;
+import com.hfy.export_cart.router.CartRouterTable;
 
 @Route(path = "/homepage/homeActivity")
 public class HomeActivity extends AppCompatActivity {
@@ -37,5 +41,13 @@ public class HomeActivity extends AppCompatActivity {
         //调用购物车组件服务：获取购物车商品数量
         TextView tvCartProductCount = findViewById(R.id.tv_cart_product_count);
         tvCartProductCount.setText("购物车商品数量:"+ CartServiceUtil.getCartProductCount().productCount);
+
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction= manager.beginTransaction();
+
+        Fragment userFragment = (Fragment) ARouter.getInstance().build(CartRouterTable.PATH_FRAGMENT_CART).navigation();
+        transaction.add(R.id.fl_test_fragment, userFragment, "tag");
+        transaction.commit();
     }
 }
